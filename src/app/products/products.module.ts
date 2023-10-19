513aca196150d3c18055501d7da40249810d2c44
Laura <laurapicon98@gmail.com>
2023-10-19 21:08:54 +0200

    Custom label(directiva personalizada) + Color del host(directive input + Cambiar el mensaje de la etiqueta

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -4,6 +4,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProductsRoutingModule } from './products-routing.module';
 import { ProductPageComponent } from './pages/product-page/product-page.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
@@ -16,6 +17,7 @@ import { ProductPageComponent } from './pages/product-page/product-page.componen
     CommonModule,
     ProductsRoutingModule,
     ReactiveFormsModule,
+    SharedModule,
   ]
 })
 export class ProductsModule { }
