972d9bb6d5a964d8fd5b479c633f3ac5bd577955
Laura <laurapicon98@gmail.com>
2023-10-23 19:15:41 +0200

    Inicio sección Signals + sideMenu

---
+++ src/app/signals/components/side-menu/side-menu.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+
+@Component({
+  selector: 'side-menu',
+  templateUrl: './side-menu.component.html',
+  styleUrls: ['./side-menu.component.css']
+})
+export class SideMenuComponent {
+
+  public menuItem: MenuItem[]= [
+    { title: 'Contador', route: 'counter' },
+    { title: 'Usuario', route: 'user-info' },
+    { title: 'Mutaciones', route: 'properties' },
+  ];
+
+
+}
